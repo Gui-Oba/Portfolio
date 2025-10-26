c5d29c8d183bb8ffcba7171342505bbf55379fb8
Gui Oba <gui.msampaio@gmail.com>
2025-10-26 12:53:37 -0400

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import Home from './pages/Home.jsx';
 import Gallery from './pages/Gallery.jsx';
 import Writing from './pages/Writing.jsx';
 import WritingPost from './pages/WritingPost.jsx';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import {Analytics} from '@vercel/analytics/react';
 
 export default function App() {
   return (
+    <div>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/writing" element={<Writing />} />
       <Route path="/writing/:slug" element={<WritingPost />} />
     </Routes>
+    <SpeedInsights />
+    <Analytics />
+    </div>
   );
 }
